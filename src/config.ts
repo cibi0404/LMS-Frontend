14d88b2618205d56b4d0e24676c449508c9c38aa
cibi0404 <158059331+cibi0404@users.noreply.github.com>
2024-06-24 20:05:06 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 //"https://lms-backend-6nx9.onrender.com/"
 
 
-const BASE_URL= "https://lms-backend-6nx9.onrender.com"
+const BASE_URL= "https://lms-backend-64vq.onrender.com"
 export default BASE_URL
